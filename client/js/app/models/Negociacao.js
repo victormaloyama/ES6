1e5b62b29709c82a6bfa187563dd885d2407abe4
Victor Maloyama <40443525+victormaloyama@users.noreply.github.com>
2018-08-02 13:52:59 -0300

    Add files via upload

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,29 @@
+class Negociacao {
+    
+    /* Firstly, we needed to get the data from the parameters and named they
+    as properties.
+    
+    It receives the data from NegociacaoController.js */
+    constructor(data, quantidade, valor) {
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+    
+    get volume() {     
+        return this._quantidade * this._valor;
+    }
+    
+    get data() {
+        return new Date(this._data.getTime());
+    }
+    
+    get quantidade() {
+        return this._quantidade;
+    }
+    
+    get valor() {
+        return this._valor;
+    }
+}
